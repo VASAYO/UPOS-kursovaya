c8b12a0b3d33d2dd596b5954e42adfca8998b99d
Reqved <Reqved@yandex.ru>
2025-01-04 18:57:15 +0300

    Fixed output (sin acquired), sin 7 mv,  WIP on harm and sin values

--- Detector/amp_restrictor_with_detector.m
+++ Detector/amp_restrictor_with_detector.m
@@ -11,25 +11,24 @@ Fc = 200e6;
 Fg = 220e6;
 Fpc = Fg - Fc;
 
-L1 = 50e-6;
-L2 = L1;
+L1 = 1e-9;
+L2 = 50e-9;
 L3 = L2;
-coupling = 0.5;
 
 Q = 30;
 
-Rloss = (2 * pi * Fpc * L1)/Q;
+Rloss = (2 * pi * Fc * L1)/Q;
 
-Cindet = 1/((2 * pi * Fpc)^2 * L1);
-C2 = Cindet/2;
+Cindet = 1/((2 * pi * Fc)^2 * L1);
+C2 = 1/((2 * pi * Fc)^2 * (L2 + L3));
 
-highBound = 1/Fpc;
 lowBound = 1/Fc;
+highBound= 1/Fmod;
 % lowbound < tetaDet < highbound, tetaDet = Rdet * Cdet
 tetaDet = (highBound - lowBound) / 2;
 Rdet = 5e3;
 Cdet = tetaDet / Rdet;
 
 % Csv >> Cdet, Lsv >> L1
-Csv = 5e-9;
-Lsv = 50e-3;
+Lsv = 100e-6;
+Csv = 120e-9;
